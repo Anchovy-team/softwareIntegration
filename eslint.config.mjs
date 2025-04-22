41b827084ce8ce0d2617d05b7ce128931241cd5d
ciugiu <corneliu.ciugurean@epita.fr>
2025-04-22 11:57:12 +0200

    chore: init

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintRecommended from '@eslint/js';
+
+export default [
+  {
+    files: ['**/*.{js,ts}'],
+
+    languageOptions: {
+      parser: tsparser,
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...eslintRecommended.rules,
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'warn',
+      'no-restricted-syntax': [
+        'error',
+        'ForInStatement',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,ts}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
