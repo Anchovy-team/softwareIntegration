f796f2b5b6675b94b749f74fcc2980c547e3d197
ciugiu <corneliu.ciugurean@epita.fr>
2025-05-03 23:36:21 +0200

    chore: Transform src/constants/messages.routes.js to TypeScript and rename to messages.routes.ts

--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
 
-const messageService = require('../controllers/messages.controller');
+const router = Router();
+
+import messageService from '../controllers/messages.controller';
 
 // here we define our routes
 router.post('/add/message', messageService.addMessage);
@@ -10,4 +11,4 @@ router.put('/edit/:messageId', messageService.editMessage);
 router.delete('/delete/:messageId', messageService.deleteMessage);
 router.get('/:messageId', messageService.getMessageById);
 
-module.exports = router;
+export default router;
