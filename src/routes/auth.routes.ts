70b38d7f93475e11defe04f03fd3f5dce3c8d088
ciugiu <corneliu.ciugurean@epita.fr>
2025-05-03 23:39:00 +0200

    chore: Transform src/constants/auth.routes.js to TypeScript and rename to auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
 
-const authServices = require('../controllers/auth.controller');
+const router = Router();
+
+import authServices from '../controllers/auth.controller';
 
 router.post('/signup', authServices.signup);
 router.post('/login', authServices.signin);
 router.get('/me', authServices.getUser);
 router.get('/logout', authServices.logout);
 
-module.exports = router;
+export default router;
