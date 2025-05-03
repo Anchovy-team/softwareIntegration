64fb1c98cf65ab8989eaa0d4c120b6d3f6062ae4
ciugiu <corneliu.ciugurean@epita.fr>
2025-05-04 01:08:15 +0200

    feat: Declared export type

--- src/models/ratingModel.ts
+++ src/models/ratingModel.ts
@@ -24,4 +24,10 @@ const ratingSchema = new mongoose.Schema(
   },
 );
 
+export interface Rating {
+  movie_id: number;
+  email: string;
+  rating: number;
+}
+
 export default mongoose.model('Rating', ratingSchema);
