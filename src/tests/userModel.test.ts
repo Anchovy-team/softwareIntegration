c5e0026e6f3560167c9882fc3d525b0c2863b245
Winnerty <timofey.n.karpeev@gmail.com>
2025-05-11 18:35:20 +0200

    test: Added error and success cases tests for user model

--- src/tests/userModel.test.ts
+++ src/tests/userModel.test.ts
@@ -18,3 +18,60 @@ afterAll(async () => {
     await mongoose.disconnect();
     await mongoServer.stop();
 });
+
+describe('User model', () => {
+    it('should create a valid user', async () => {
+        const user = await User.create({
+            username: 'username',
+            email: 'test@epita.fr',
+            password: 'admin',
+        });
+  
+        expect(user).toBeDefined();
+        expect(user.email).toBe('test@epita.fr');
+        expect(user.username).toBe('username');
+    });
+  
+    it('should fail without an email', async () => {
+        try {
+            await User.create({
+                username: 'noemail',
+                password: 'password',
+            });
+        } catch (err: any) {
+            expect(err).toBeDefined();
+            expect(err.errors.email).toBeDefined();
+        }
+    });
+  
+    it('should fail without a password', async () => {
+        try {
+        await User.create({
+            username: 'nopassword',
+            email: 'test@epita.fr',
+        });
+        } catch (err: any) {
+            expect(err).toBeDefined();
+            expect(err.errors.password).toBeDefined();
+        }
+    });
+  
+    it('should fail with duplicate email', async () => {
+        await User.create({
+            username: 'user1',
+            email: 'duplicate@epita.fr',
+            password: 'admin1',
+        });
+
+        try {
+        await User.create({
+            username: 'user2',
+            email: 'duplicate@epita.fr',
+            password: 'admin2',
+        });
+        } catch (err: any) {
+            expect(err).toBeDefined();
+            expect(err.code).toBe(11000);
+        }
+    });
+});
